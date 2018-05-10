a9b2493b7833a807e1aa7a6a0183e06a2260ef6e
Mahmoud Bakhshinejad <mahmoud.bakhshinejad@gmail.com>
2018-05-10 21:53:38 +0430

    Re-structured and shared Device struct

---
+++ src/vendor/data/data.go
@@ -0,0 +1,11 @@
+// Device
+package data
+
+// struct for storing a device object
+type Device struct {
+	Id          string `json:"id"`
+	DeviceModel string `json:"deviceModel"`
+	Name        string `json:"name"`
+	Note        string `json:"note"`
+	Serial      string `json:"serial"`
+}
